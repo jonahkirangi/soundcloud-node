877b925df66c7420705e51e97884745d77ac9735
Jonah Kirangi <jonahkirangi@gmail.com>
2014-11-30 00:07:46 -0800

    Add genres and select list

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,26 @@
-SC.initialize({
-  client_id: "680a993566504ca7b4d23059b96ae53b"
-});
-
-$(document).ready(function() {
-  SC.get('/tracks/293', function(track, error) {
-    if (error) {
-      alert("Error: " + error.message);
-    } else {
-      $('#player').html(SC.oEmbed(track.permalink_url, document.getElementById('player')));
+function playSomeSound(genre) {
+  SC.get("/tracks", {
+    genres: genre,
+    bpm: {
+      from: 100
     }
+  }, function(tracks) {
+    var random = Math.floor(Math.random() * 49);
+    SC.oEmbed(tracks[random].uri, { auto_play: true }, document.getElementById("target"));
   });
-});
+}
+
+window.onload = function() {
+  SC.initialize({
+    client_id: "680a993566504ca7b4d23059b96ae53b"
+  });
+
+  var menuLinks = document.getElementsByClassName("genre");
+  for (var i = 0; i < menuLinks.length; i++) {
+    var menuLink = menuLinks[i];
+    menuLink.onclick = function(e) {
+      e.preventDefault();
+      playSomeSound(menuLink.innerHTML);
+    };
+  }
+};
